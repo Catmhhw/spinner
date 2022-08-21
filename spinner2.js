e30c78018258505a45bda8b4568fe42f14e7c30c
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-21 19:54:54 -0400

    add function to loop spin

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,21 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
+const args = process.argv.slice(2);
 
-let spin = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ','\r|  ', '\r/  ', '\r-  ', '\r\\  ']
+let spin = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ']
 let time = 100
 
-for (const part of spin) {
-    setTimeout(() => {
+function callback() {
+    for (const part of spin) {
+        setTimeout(() => {
             process.stdout.write(part);
         }, time);
         time += 200;
+    }
 }
+
+const loop = (loopAmount) => {
+    for (let i = 0; i < loopAmount; i++) {
+        callback();
+    }
+}
+
+loop(args);
