99c4427c3b6cb5bd91e6a077792eb520d6a3211f
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-21 23:00:46 -0400

    refactor function

--- spinner2.js
+++ spinner2.js
@@ -3,18 +3,14 @@ const args = process.argv.slice(2);
 let spin = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ']
 let time = 100
 
-function callback() {
-    for (const part of spin) {
-        setTimeout(() => {
-            process.stdout.write(part);
-        }, time);
-        time += 200;
-    }
-}
-
 const loop = (loopAmount) => {
     for (let i = 0; i < loopAmount; i++) {
-        callback();
+        for (const part of spin) {
+            setTimeout(() => {
+                process.stdout.write(part);
+            }, time);
+            time += 200;
+        }
     }
 }
 
